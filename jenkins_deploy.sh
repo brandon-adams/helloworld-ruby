b18af00b54b1aa3b7ad8af6793d31c7bb1d67bda
Brandon <brandon-adams@users.noreply.github.com>
2014-08-15 15:39:55 -0400

    Update jenkins_deploy.sh

--- jenkins_deploy.sh
+++ jenkins_deploy.sh
@@ -3,5 +3,5 @@
 cf login -a http://api.192.168.4.14.xip.io -u admin -p password -o newwave -s dev
 cf push --no-start
 APP_NAME=`cat manifest.yml | grep -i name | awk '{ print $3 }'`
-cf restage $APP_NAME
+#cf restage $APP_NAME
 cf start $APP_NAME
